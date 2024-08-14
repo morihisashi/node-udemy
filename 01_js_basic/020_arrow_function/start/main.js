04419aeafe80f7b7310ae33c5b4273a5315dccd3
morihisashi <hisa.1995.322@outlook.com>
2024-08-14 22:12:16 +0900

    アロー関数について学ぼう2

--- 01_js_basic/020_arrow_function/start/main.js
+++ 01_js_basic/020_arrow_function/start/main.js
@@ -3,6 +3,14 @@ function fn(number) {
 }
 console.log(fn(2));
 
-const fnArrow = number =>  number * 2;
+const fnArrow = number =>  {
+  console.log(number);
+  return number * 2;
+}
+
+const fnArrowObj = number => ({
+  result: number * 2
+})
 
 console.log(fnArrow(2));
+console.log(fnArrowObj(2));
