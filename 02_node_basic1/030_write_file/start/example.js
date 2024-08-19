9998d9a802ab176c177003ccb1f1e9dd2dc6da54
morihisashi <hisa.1995.322@outlook.com>
2024-08-19 23:30:39 +0900

    パスについて学ぼう

--- 02_node_basic1/030_write_file/start/example.js
+++ 02_node_basic1/030_write_file/start/example.js
@@ -1,1 +1,8 @@
-console.log('hello, node.js');
+const fs = require('fs');
+const path = require('path');
+
+const distPath = path.resolve(__dirname, '../dist/text.txt');
+console.log(distPath);
+// console.log(__dirname + '../dist/test.txt');
+
+fs.writeFileSync(distPath, 'hello, node.js');
