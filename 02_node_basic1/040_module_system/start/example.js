0527b6e27ccf14de65636c5ed1cfde70099d81d9
morihisashi <hisa.1995.322@outlook.com>
2024-08-21 23:31:36 +0900

    commonJSのモジュールについて学ぼう

--- 02_node_basic1/040_module_system/start/example.js
+++ 02_node_basic1/040_module_system/start/example.js
@@ -1,6 +1,8 @@
-function plus(a, b) {
-  return a + b;
-}
+// const calc = require('./calc');
+// console.log(calc);
+
+const {plus} = require('./calc');
 const result = plus(1, 2);
 console.log(result);
-console.log(result);
+// const result2 = calc.minus(1, 2);
+// console.log(result2);
