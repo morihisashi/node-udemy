6b2fffcce842c2f1dc0f064e1f437272b7825d78
morihisashi <hisa.1995.322@outlook.com>
2024-08-26 23:19:44 +0900

    commit npm

--- 03_npm/010_intro/start/main.mjs
+++ 03_npm/010_intro/start/main.mjs
@@ -1,2 +1,9 @@
+import _ from "../lib/lodash.js";
+
 const original = { prop: { nested: "value" } };
 // オブジェクトの複製
+const cloned = _.cloneDeep(original);
+
+cloned.prop.nested = "new value";
+
+console.log(original, cloned);
