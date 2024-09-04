3782542cfdc7d04def42a509e10a3990dca2cc6c
morihisashi <hisa.1995.322@outlook.com>
2024-09-04 23:01:31 +0900

    locatorの使い方

--- 04_automation/010_playwright/040_extract_data/start/Example.mjs
+++ 04_automation/010_playwright/040_extract_data/start/Example.mjs
@@ -4,9 +4,14 @@ import { chromium } from "@playwright/test";
   const browser = await chromium.launch({ headless: false, slowMo: 500 });
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
+  // css セレクター
   const htmlStr = await page.content();
-
-  console.log(htmlStr);
-
+  const pageTitleLocator = page.locator('.navbar-brand');
+  const pageTitle = await pageTitleLocator.innerText();
+  // console.log(pageTitle);
+  // 文字列で要素を取得
+  const textLocator = page.locator("text=名刺管理アプリ");
+  const pageText = await textLocator.innerText();
+  console.log(pageText);
   await browser.close();
 })();
