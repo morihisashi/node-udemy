523b2a15ab8e6f61387cd6e6beff9e1cc7519665
morihisashi <hisa.1995.322@outlook.com>
2024-09-08 09:49:23 +0900

    UIイベントのスクリプトを記述しよう

--- 04_automation/010_playwright/047_ui_events/start/Example.mjs
+++ 04_automation/010_playwright/047_ui_events/start/Example.mjs
@@ -8,12 +8,18 @@ import { chromium } from "@playwright/test";
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
 
+  const inputLocator = page.locator("//*[@id=\"__next\"]/div/div[1]/label/input");
+  await inputLocator.type('美');
+
+  const pager3Locator = page.locator(".page-link.page-number >> nth=2");
+  await pager3Locator.click();
+
   // CSS セレクターで要素を取得
   const pageTitleLocator = await page.locator(".cards.list-group-item");
   const parentLocator = await pageTitleLocator.locator('..')
   const pageTitle = await parentLocator.innerHTML();
   console.log(pageTitle);
 
-  await browser.close();
+  // await browser.close();
 
 })();
