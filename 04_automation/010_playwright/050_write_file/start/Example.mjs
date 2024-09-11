8f9e0626d8a906afb6ca6ad1846941218ca40426
morihisashi <hisa.1995.322@outlook.com>
2024-09-11 21:11:23 +0900

    ファイルへ書き込み処理を記述してみよう

--- 04_automation/010_playwright/050_write_file/start/Example.mjs
+++ 04_automation/010_playwright/050_write_file/start/Example.mjs
@@ -1,4 +1,5 @@
 import { chromium } from "@playwright/test";
+import * as fs from "fs";
 
 (async () => {
   const browser = await chromium.launch({ headless: false, slowMo: 500 });
@@ -6,7 +7,9 @@ import { chromium } from "@playwright/test";
   await page.goto("http://localhost:3000");
 
   const cardLocator = page.locator(".cards.list-group-item >> nth=1");
-
+  const cardText = await cardLocator.textContent();
   await browser.close();
+
+  fs.writeFileSync("./text-data.csv", cardText);
 
 })();
