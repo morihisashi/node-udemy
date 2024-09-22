b9f6432cf14354046f099a4855f2f75ea280f3ed
morihisashi <hisa.1995.322@outlook.com>
2024-09-23 08:24:34 +0900

    ファイル変更

--- 04_automation/020_google_sheet/015_debug/start/Example.mjs
+++ 04_automation/020_google_sheet/015_debug/start/Example.mjs
@@ -3,7 +3,7 @@ import env from 'dotenv';
 env.config();
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const secrets = require('../../../google_secrets.json');
+const secrets = require('../../../udemy-test-436414-d4baa72f756b.json');
 
 
 (async () => {
@@ -23,6 +23,7 @@ const secrets = require('../../../google_secrets.json');
     const b1 = sheet.getCell(0,1);
     const b2 = sheet.getCellByA1('B2');
 
+    // コメント
     console.log('a1', a1.value);
     console.log('b1', b1.value);
     console.log('b2', b2.value);
